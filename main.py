d4fdc7f435e385ec93df55ecc9c9341bef94169f
LiamEvers02 <liamevers14@gmail.com>
2025-08-17 17:52:44 +1200

    Fixed bug where recipients weren't read in properly

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
     reader = Reader()
     participants = reader.get_participants()
     for participant in participants:
+        print()
         print(participant)
         print("-" * 40)  # Separator for readability
--- reader.py
+++ reader.py
@@ -87,31 +87,30 @@ class Reader:
                     ethnicity=row['Unnamed: 37'],
                     )
                 
-                if row["After reading the description above, which option(s) appeal to you?"] == "Yes Yes Yes!":
-                    donor.homeFertility = 3
-                elif row["Unnamed: 12"] == "Perhaps":
-                    donor.homeFertility = 2
+                if row["After reading the description above, which option(s) appeal to you?"] != None:
+                    recipient.homeFertility = 3
+                elif row["Unnamed: 12"] != None:
+                    recipient.homeFertility = 2
                 else:
-                    donor.homeFertility = 1
-                
-                if row["Unnamed: 14"] == "Yes Yes Yes!":
-                    donor.privateDonor = 3
-                
-                elif row["Unnamed: 15"] == "Perhaps":
-                    donor.privateDonor = 2
-                else:
-                    donor.privateDonor = 1
+                    recipient.homeFertility = 1
 
-                if row["What level of involvement would you want with any child born through this process?"] == "Full, shared parental responsibility and rights with the recipient and their partner":
-                    donor.involvementLevel = 3
-                elif row["Unnamed: 18"] == "Being a known figure in the child’s life without parental responsibilities and rights":
-                    donor.involvementLevel = 2
+                if row["Unnamed: 14"] != None:
+                    recipient.privateDonor = 3
+                elif row["Unnamed: 15"] != None:
+                    recipient.privateDonor = 2
                 else:
-                    donor.involvementLevel = 1
+                    recipient.privateDonor = 1
 
-                donor.setCriteriaText = row["Sperm Positive is used by heterosexual and LGBTI+ singles and couples. Please specify any preferences or other criteria, such as cross-cultural or religious restrictions."]
-                donor.awesomeText = row["What makes you awesome?"]
-                donor.medicalText = row["Please give details of your medical history, or family medical history, that may be relevant."]
-                donor.moreText = row["Anything else you'd like to share?"]
+                if row["What level of involvement would you want with any child born through this process?"] != None:
+                    recipient.involvementLevel = 3
+                elif row["Unnamed: 18"] != None:
+                    recipient.involvementLevel = 2
+                else:
+                    recipient.involvementLevel = 1
+
+                recipient.criteriaText = row["Sperm Positive is used by heterosexual and LGBTI+ singles and couples. Please specify any preferences or other criteria, such as cross-cultural or religious restrictions."]
+                recipient.awesomeText = row["What makes you awesome?"]
+                recipient.medicalText = row["Please give details of your medical history, or family medical history, that may be relevant."]
+                recipient.moreText = row["Anything else you'd like to share?"]
                 participants.append(recipient)
         return participants
